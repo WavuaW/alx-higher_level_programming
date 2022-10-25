2a67a9c5d9864ffd22e9119a6b0458540c4ce97d
WavuaW <wavuamuroka@gmail.com>
2022-10-25 20:05:08 +0300

    Python input output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,11 +5,4 @@
 def read_file(filename=""):
     """Prints the contents of a UTF8 text file"""
     with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")#!/usr/bin/python3
-"""This module defines a text file-reading function"""
-
-
-def read_file(filename=""):
-    """Prints the contents of a UTF8 text file"""
-    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
