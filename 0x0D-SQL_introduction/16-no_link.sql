805ec34b098d78a7327a2d51f6a789fb7acb16e1
WavuaW <wavuamuroka@gmail.com>
2022-11-29 21:26:51 +0300

    16. Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
--- lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
--- The result should display: the score, the number of records for this score with the label number
--- The list should be sorted by the number of records (descending)
--- The database name will be passed as an argument to the mysql command
-
-SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
+-- Lists all records of the table second_table having a name value.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
