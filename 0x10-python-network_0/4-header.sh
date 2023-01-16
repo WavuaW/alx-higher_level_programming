9b8e1c4ce5bc0c8caa6f05907df07a6d0a70f85a
WavuaW <wavuamuroka@gmail.com>
2023-01-16 21:29:42 +0300

    correction to headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# display all HTTP methods the server will accept using curl.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# send a GET request to the URL using curl, and display the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
